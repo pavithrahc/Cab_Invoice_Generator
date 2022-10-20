4c06ff26402c9de944a096e498ea2e9bd4c7930d
Pavithrahc <pavithramubbu93@gmail.com>
2022-10-20 21:41:31 +0530

    Added UC2: Multiple Ride

--- src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
@@ -20,5 +20,12 @@ public class InvoiceGeneratorTest {
         double fare = invoiceGenerator.calculateFare(distance, time );
         Assert.assertEquals(5,fare,0.0);
     }
+    @Test
+    public void givenDistanceAndTime_shouldReturnTotalFareCalculate(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, fare,0.0);
+    }
 
 }
